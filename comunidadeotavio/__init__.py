704648b9240689225b33cce9e29c07763e92d3de
@otavioferrazdearaujo <30276248+88otavio@users.noreply.github.com>
2023-08-27 14:39:36 -0300

    Ajuste de variavel de ambiente para usar o DB do railway (postgreSQL)

--- comunidadeotavio/__init__.py
+++ comunidadeotavio/__init__.py
@@ -2,12 +2,15 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-
+import os
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '29cecf8afd6176f06bb3f55472d490d1'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
+if var_db := os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = var_db
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
 database = SQLAlchemy(app)
 
