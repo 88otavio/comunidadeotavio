20c37a01c20ea957424ad38732a048421f79cf51
@otavioferrazdearaujo <30276248+88otavio@users.noreply.github.com>
2023-08-27 15:12:55 -0300

    Ajuste para criar o DB no reailway

--- comunidadeotavio/__init__.py
+++ comunidadeotavio/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 app = Flask(__name__)
 
@@ -20,4 +21,14 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'alert-info'
 
 
+from comunidadeotavio import models
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada")
+else:
+    print('Base de dados já existe')
 from comunidadeotavio import routes
