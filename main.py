07891d4d4f4d6f76df0875ca96625216ad1691c3
@otavioferrazdearaujo <30276248+88otavio@users.noreply.github.com>
2023-08-27 14:29:49 -0300

    Upload dos arquivos do site

---
+++ main.py
@@ -0,0 +1,17 @@
+from comunidadeotavio import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# no read-me descrever
+# o que o projeto faz
+# as tecnologias utilizadas
+# explicação mais longa com divisão da estrutura do projeto
+# from datetime import datetime
+#
+
+# Rode isso no console do pychram que ele cria o database (cuidado: vai excluir _todo o databese se existir um
+# from comunidadeotavio import app, database
+# with app.app_context():
+#     database.drop_all()
+#     database.create_all()
